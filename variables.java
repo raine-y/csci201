45efae82cf5fbbc0df7b09f406a0d5eb474b1b8f
Raine <rainehwalker@proton.me>
2025-08-25 14:33:35 -0400

    variables,,, again

---
+++ variables.java
@@ -0,0 +1,13 @@
+public class variables {
+    public static void main(String[] args) {
+        int number; 
+        String name; // 32bit space allocated 
+        number = 73;
+        name = new String("raine walker"); // Declared on HEAP vs stack
+        System.out.println("number = " + number + ", name = " + name);
+    }
+}
+
+/*
+ *  A variable is a name given to a piece of data to call upon it
+ */
