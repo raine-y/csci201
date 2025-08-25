b818e7187be215de409b7fbd311ac189ad3c9e0f
Raine <rainehwalker@proton.me>
2025-08-25 14:18:32 -0400

    hello, again, friend of a friend

---
+++ hello.java
@@ -0,0 +1,11 @@
+public class hello { // Class <--
+    
+    public static void main(String[] /* <-- String array */ args) { // <-- static (no object) 
+        System.out.println("Hello, world!");                      // vs instance method
+    }
+
+}
+
+/* 
+ * 
+ */
