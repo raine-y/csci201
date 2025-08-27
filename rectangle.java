e96b3018fcf39c550c5bc4d4b863c74e03675d68
Raine <rainehwalker@proton.me>
2025-08-27 12:17:07 -0400

    rect

---
+++ rectangle.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class rectangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, let's calculate the area of a square!");
+    }
+} 
