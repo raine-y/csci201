47c2ba363357c54ef31af16667d851cffa05d2d6
Raine <rainehwalker@proton.me>
2025-09-03 14:33:52 -0400

    Mathmatical..........fuckme

--- circle.java
+++ circle.java
@@ -3,11 +3,13 @@ import java.util.Scanner;
 public class circle {
     public static void main(String[] args) {
         System.out.print("Please enter the radius of your circle: ");
+        final Double PI = Math.PI; // final = const, usually capitalized + Math class
         Scanner scanner = new Scanner(System.in);
         Double radius = scanner.nextDouble();
-        Double circumference = radius * 3.14 * 2; 
-        Double area = radius * radius * 3.14;
+        Double circumference = radius * PI * 2; 
+        Double area = Math.pow(radius, 2) * PI;
         System.out.println("Radius = " + radius);
+        System.out.println("Pi = " + PI);
         System.out.println("Circumference = " + circumference);
         System.out.println("Area = " + area);
         scanner.close();
