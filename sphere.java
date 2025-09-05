34dbdb5af379a6dcafedeb579940643cd51f5aff
Raine <rainehwalker@proton.me>
2025-09-05 14:48:56 -0400

    yellow :P

--- sphere.java
+++ sphere.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class sphere {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String YELLOW_UNDERLINED = "\033[4;33m";
     public static void main(String[] args){
         final Double PI = Math.PI;
         System.out.print("Enter the radius of your sphere: ");
@@ -10,8 +12,8 @@ public class sphere {
         Double volume = (4.0 / 3.0) * PI * Math.pow(radius, 3);
         System.out.println("Radius = " + radius);
         System.out.println("Pi = " + PI);
-        System.out.println("Surface Area = " + surfaceArea);
-        System.out.println("Volume = " + volume);
+        System.out.println("Surface Area = " + YELLOW_UNDERLINED + surfaceArea + ANSI_RESET);
+        System.out.println("Volume = " + YELLOW_UNDERLINED + volume + ANSI_RESET);
         scanner.close();
     }
 }
