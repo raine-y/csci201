fc046b22ff9f20c7fb00ab5f29e41b311a29618d
Raine <rainehwalker@proton.me>
2025-09-14 16:44:22 -0400

    YELLOW

--- seconds.java
+++ seconds.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 public class seconds {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String YELLOW_UNDERLINED = "\033[4;33m";
     public static void main(String[] args) {
         System.out.println("Time Duration in Seconds!!!");
         System.out.println("~");
-        System.out.println("[Format your entry as: 00:00:00]");
-        System.out.println("[As hours, minutes and seconds ]");
+        System.out.println("[Format your entry as: " + YELLOW_UNDERLINED + "00:00:00" + ANSI_RESET + "]");
+        System.out.println("[----hours:minutes:seconds-----]");
         System.out.println("~");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Time to convert: ");
@@ -17,7 +19,7 @@ public class seconds {
         int minutes = Integer.parseInt(timeArray[1]);
         int seconds = Integer.parseInt(timeArray[2]);
         int timeSeconds = (hours * 3600) + (minutes * 60) + seconds;
-        System.out.println(hours + ":" + minutes + ":" + seconds + " = " + timeSeconds + " seconds");
+        System.out.println(hours + ":" + minutes + ":" + seconds + " = " + YELLOW_UNDERLINED + timeSeconds + ANSI_RESET + " seconds");
         scanner.close();
     }
 }
