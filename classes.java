37fcb52b59ae5a4f2877e0a3cf9d70dc3da6ee06
Raine <rainehwalker@proton.me>
2025-09-17 15:03:49 -0400

    class ovr

---
+++ classes.java
@@ -0,0 +1,7 @@
+public class classes {
+    public static void main(String[] args) {
+
+    }
+}
+
+// Static methods (func.) (i.e: math) V.S.Instant methods (meth.) (must create an object) (i.e: random)
