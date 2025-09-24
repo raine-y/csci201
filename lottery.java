8efb486662a3f7c0b979e9773bcf648e4c8e03a0
Raine <rainehwalker@proton.me>
2025-09-24 12:29:08 -0400

    removed projects package... file structure...

--- lottery.java
+++ lottery.java
@@ -1,5 +1,3 @@
-package projects;
-
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Random;
@@ -8,8 +6,6 @@ public class lottery {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
-        // for (int i = 0; i < 3; i++){
-        // }
         System.out.println("Please enter your lottery numbers, as such: '1,2,3',");
         System.out.println("to see if you have wom the lottery!!!");
         System.out.print("Enter #s here: ");
@@ -23,6 +19,7 @@ public class lottery {
         int[] lottoStream = random.ints(3, 1, 25).toArray();
         System.out.println("The winning lottery numbers are: " + Arrays.toString(lottoStream));
         System.out.println("*     Your lottery numbers were: " + Arrays.toString(playerArray));
+        System.out.println("~");
         if ( lottoStream[0] == player1) {
             if ( lottoStream[1] == player2) {
                 if ( lottoStream[2] == player3) {
