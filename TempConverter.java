6e2eccb4e8d010511878a372f9135c42f69e9cc3
Raine <rainehwalker@proton.me>
2025-09-24 13:09:36 -0400

    quiz 2 '100%'

--- TempConverter.java
+++ TempConverter.java
@@ -1,4 +1,4 @@
-package projects;
+
 import java.util.Scanner;
 
 public class TempConverter {
---
+++ quizTwo.java
@@ -0,0 +1,27 @@
+public class quizTwo {
+    public static void main(String[] args) {
+        int i21 = 36 / 6*2;
+        System.out.println("1: " + i21);
+        int i20 = 36 / 6/2;
+        System.out.println("2: " + i20);
+        int i18 = 3, i19 = 2; double d4 = i18 / (double) i19;
+        System.out.println("3: " + d4);
+        System.out.println("4: [");
+        char c2 = 66; --c2; System.out.println(c2);
+        System.out.println("]");
+        int i22 = 4 + 36 % 10;
+        System.out.println("5: " + i22);
+        System.out.println("6: [");
+        int i25 = 1, i26 = 2; String st2 = i25 + i26 + "@"; System.out.println(st2);
+        System.out.println("]");
+        System.out.println("7: [");
+        int i23 = 1, i24 = 2; String st1 = "@" + i23 + i24; System.out.println(st1);
+        System.out.println("]");
+        int i16 = 3, i17 = 2; double d3 = (double) i16 / i17;
+        System.out.println("8: " + d3);
+        System.out.println("9: [");
+        char c1 = 65; System.out.println(c1);
+        System.out.println("]");
+
+    }
+}
