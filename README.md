fc8dea035a37ab6ef0fbfbd7369d5385e4bdd76b
Raine <rainehwalker@proton.me>
2025-10-10 14:40:56 -0400

    good star-t  ... i finsihed

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
  1   1   1   1   1   1   1   1   1   1    1    1
 X^1 X^2 X^3 X^4 X^5 X^6 X^7 X^8 X^9 X^10 X^11 X^12 </= 2^12-1
  1   2   4   8  16  32  64  128 256 512  1024 2048
+
+ ## exam 1
+ Q1. switch case
+ Q2. Nested if-else to if-else-if-else
+
+ 
+ Q5. Nested loops
--- Stars.java
+++ Stars.java
@@ -1,12 +1,49 @@
 public class Stars {
     public static void main(String[] args) {
         final int MAX_ROWS = 10;
+        final int HALF_ROWS = 5;
         System.out.println("a.");
+        System.out.println("");
         for (int row = 1; row <= MAX_ROWS; row++) {
             for (int star = 10; star >= row; star--)
             System.out.print("*");
             System.out.println();
         }
+        System.out.println("");
         System.out.println("b.");
+        System.out.println("");
+         for (int row = 1; row <= MAX_ROWS; row++) {
+            for (int space = 9; space >= row; space--)
+            System.out.print(" ");
+            for (int star = 1; star <= row; star++)
+            System.out.print("*");
+            System.out.println();
+        } 
+        System.out.println("");
+        System.out.println("c.");
+        System.out.println("");
+        for (int row = 1; row <= MAX_ROWS; row++) {
+            for (int space = 2; space <= row; space++)
+            System.out.print(" ");
+            for (int star = 10; star >= row; star--)
+            System.out.print("*");
+            System.out.println();
+        } 
+        System.out.println("");
+        System.out.println("d.");
+        System.out.println("");
+        for (int row = 1; row <= HALF_ROWS; row++) {
+            for (int space = 4; space >= row; space--)
+            System.out.print(" ");
+            for (int star = 1; star <= row * 2 - 1; star++)
+            System.out.print("*");
+            System.out.println();
+        } for (int row = 1; row <= HALF_ROWS; row++) {
+            for (int space = 2; space <= row; space++)
+            System.out.print(" ");
+            for (int star = 9; star >= row * 2 - 1; star--)
+            System.out.print("*");
+            System.out.println();
+        }
     }
 }
