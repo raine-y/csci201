476ac59250161c5090c6463b5d9036d16b7c06e3
Raine <rainehwalker@proton.me>
2025-10-23 17:13:11 -0400

    global constants

--- method.java
+++ method.java
@@ -4,9 +4,8 @@ public class method {
 
     public static final String YELLOW_BOLD = "\033[1;33m";
     public static final String YELLOW_UNDERLINED = "\033[4;33m";
-    public static final String RESET = "\033[0m";
-    final static double PI = Math.PI; // Global constants ONLY
-                                      // Don't write spaghetti code
+    public static final String RESET = "\033[0m"; // Global constants ONLY
+                                                  // Don't write spaghetti code
 
     public static void main(String[] args) {
 
