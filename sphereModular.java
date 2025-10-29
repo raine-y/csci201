0afcd338f16832037428caeb05ae62f7661cbad9
Raine <rainehwalker@proton.me>
2025-10-29 15:03:57 -0400

    Spheres n such

---
+++ sphereModular.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sphereModular {
+    public static void main(String[] args) {
+        displayIntro();
+        userInput();
+    }
+    public static void displayIntro() {
+        System.out.println("| ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ |");
+        System.out.println("|      + SPHERE CALCULATOR +     |");
+        System.out.println("| ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ |");
+    }
+    public static void userInput() {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
