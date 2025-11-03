1964beff612f95a859d633a219c1505e0adccc3d
Raine <rainehwalker@proton.me>
2025-11-03 17:38:59 -0500

    psychosis

--- Dealer.java
+++ Dealer.java
@@ -2,21 +2,20 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Dealer {
+
     public static void main(String[] args) {
-
         ArrayList<String> hand = new ArrayList<>(); // prevents duplicates to mirror 52 + 2 card deck
-
         for (int i = 0; i < 5; i++) {
 
             int c = i + 1; // card counter
 
             if ((Collections.frequency(hand, "Joker")) < 2) { // checks if still jokers in deck
 
-                int jokerCheck = (int) (Math.random() * (54 - hand.size()));
-
-                if (jokerCheck == 1) {
+                int jokerCheck = (int) (Math.random() * (54 - hand.size())); // Struggled to implement this in driver :/
+                                                                             // It's like the dealer has them in his pocket
+                if (jokerCheck == 1) {                                       // And uses a random number guesser to see if he plays it
                     System.out.print(c + ". ");
-                    System.out.println("Dealt card: Joker");
+                    System.out.println("Dealt card: Joker");              // Spaghetti solution
                     hand.add("Joker");
                 } else if (jokerCheck != 1) {
                     Card card = new Card();
