12136260fd1f70ff50ee2094a9601b32de96bf82
Raine <rainehwalker@proton.me>
2025-11-03 17:04:23 -0500

    works as intended

--- Card.java
+++ Card.java
@@ -10,6 +10,5 @@ public class Card {
         this.suit = suits[suitIndex];
         this.rank = ranks[rankIndex];
         return this.rank + " of " + this.suit;
-        // return "joker" + " ";
     }
 }
--- Dealer.java
+++ Dealer.java
@@ -1,21 +1,43 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Dealer {
     public static void main(String[] args) {
-        ArrayList<String> hand = new ArrayList<>(); // prevents duplicates to mirror 54 card deck
+
+        ArrayList<String> hand = new ArrayList<>(); // prevents duplicates to mirror 52 + 2 card deck
+
         for (int i = 0; i < 5; i++) {
+
             int c = i + 1; // card counter
-            System.out.print(c + ". ");
-            int jokerCheck = (int) (Math.random() *  (54 - hand.size()));
-            if (jokerCheck == 1) {
-                System.out.println("Dealt card: Joker");
-            } else if (jokerCheck != 1) {
+
+            if ((Collections.frequency(hand, "Joker")) < 2) { // checks if still jokers in deck
+
+                int jokerCheck = (int) (Math.random() * (54 - hand.size()));
+
+                if (jokerCheck == 1) {
+                    System.out.print(c + ". ");
+                    System.out.println("Dealt card: Joker");
+                    hand.add("Joker");
+                } else if (jokerCheck != 1) {
+                    Card card = new Card();
+                    String newCard = card.Deck(card.suit, card.rank);
+                    if (hand.contains(newCard) == true) {
+                        i--;
+                        continue;
+                    } else {
+                        System.out.print(c + ". ");
+                        System.out.println("Dealt card: " + newCard);
+                        hand.add(newCard);
+                    }
+                }
+            } else { // if there are no jokers
                 Card card = new Card();
                 String newCard = card.Deck(card.suit, card.rank);
                 if (hand.contains(newCard) == true) {
                     i--;
                     continue;
                 } else {
+                    System.out.print(c + ". ");
                     System.out.println("Dealt card: " + newCard);
                     hand.add(newCard);
                 }
